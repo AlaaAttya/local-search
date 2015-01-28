9909535be35d3293f41ff4fa425bbcbe82b3b4df
Alaa Attya <vidooman@gmail.com>
2015-01-29 01:12:40 +0200

    Updated: login form customization
    Signed-off-by: Alaa Attya <vidooman@gmail.com>

--- src/Dalilak/UserBundle/DalilakUserBundle.php
+++ src/Dalilak/UserBundle/DalilakUserBundle.php
@@ -4,6 +4,8 @@ namespace Dalilak\UserBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
-class DalilakUserBundle extends Bundle
-{
+class DalilakUserBundle extends Bundle {
+	public function getParent() {
+        return 'FOSUserBundle';
+    }
 }
